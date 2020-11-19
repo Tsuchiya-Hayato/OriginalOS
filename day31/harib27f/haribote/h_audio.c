3acd9331e3b1f76ff07251f9929ba8d2ce092876
unknown <hayato14399@gmail.com>
2020-11-20 05:37:17 +0900

    day31

--- day31/harib27f/haribote/h_audio.c
+++ day31/harib27f/haribote/h_audio.c
@@ -3,8 +3,8 @@
 
 int main(){
   volatile struct hdaduio_mmio *mmio = (volatile struct hdaduio_mmio *) BAR0;
-  iss = (mmio->gcap >> 8) & 0xf;
-  oss = (mmio->gcap >> 12) & 0xf;
+  int iss = (mmio->gcap >> 8) & 0xf;
+  int oss = (mmio->gcap >> 12) & 0xf;
   int x = (0x80 + iss * 0x20);
   int y = (0x80 + iss * 0x20) + (oss*0x20);
   volatile struct hdaudio_input_stream_descriptor *input_desc = BAR0 + x;
